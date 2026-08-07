12d95da90a7a2df19d8b990aaefe531d7dec082e
AwesomeNeon <ionawass11@gmail.com>
2026-08-07 20:59:21 +0100

    Add files via upload

---
+++ lesson7.py
@@ -0,0 +1,30 @@
+score = 0
+
+quiz = (
+    (
+        "1.which keyword is used to define a function in python?",
+        ("func","define","def",function),
+        3,
+    ),
+    
+    "2. Which data type is used to store True or False values?",
+    ("int","bool","str","float"),
+    2,
+),
+("3.Which symbol is used for comment in Python?", ("//","#",)"/*","--"),2),
+("4. Which function is used to display output?",
+ ("display()","show()","print","output()"),
+ 3,
+),
+(
+    "5. Which loop is used when you know the number of iterations?",
+    ("while","repeat","for","do-while"),
+    3,
+),
+("6. Which of these is a mutable data type?",
+ ("Tuple","String", "List", "Integer"),
+ 3),
+
+("7. Which brackets are used to create a tuple?",
+ ("{}","[]"."()","<>"),)
+3),
